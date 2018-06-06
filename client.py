d516a1c2d04f29564508eab64a494f38c67a548c
root <root@localhost.localdomain>
2018-06-06 19:53:15 +0200

    First commit

---
+++ client.py
@@ -0,0 +1,55 @@
+import socket
+import threading
+import sys
+import os
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# host = input("Enter host: ")
+host = socket.gethostname()
+name = str(input("Enter name: "))
+port = int(sys.argv[1])
+
+s.connect((host, port))
+os.system('clear')
+print('Connected to', host)
+print("Commands:\n- \"clear()\"\n- \"exit\"")
+s.send(name.encode())
+s.send(str("User connected: " + name).encode())
+
+
+def unos():
+    try:
+        while True:
+            inp = str(input(name + ": "))
+            z = name + ": " + inp
+            if (inp == "exit"):
+                s.send(str("User disconnected: " + name).encode())
+                s.close()
+                exit(0)
+            elif inp == "clear()":
+                os.system("clear")
+            else:
+                s.send(z.encode())
+    except Exception as e:
+        s.send(str("User disconnected: " + name).encode())
+        print(e)
+        exit(0)
+
+
+def read():
+    while True:
+        msg = str(s.recv(1024).decode())
+        if len(msg):
+            print("\n" + msg)
+
+
+t1 = threading.Thread(target=unos)
+t2 = threading.Thread(target=read)
+try:
+    t1.start()
+    t2.start()
+except Exception as e:
+    print(e, end="\n\n\n\n")
+    print("Bye bye ")
+    exit(0)
---
+++ server.py
@@ -0,0 +1,60 @@
+import socket
+import threading
+import sys
+import time
+import random
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+print("Host: "+str(host))
+port = int(sys.argv[1])
+s.bind((host, port))
+
+s.listen(10)
+users = []
+print("Server initialised on port " + str(port))
+print("Waiting for connections...")
+
+
+def accConns(s):
+    while True:
+        c, addr = s.accept()
+        name = str(c.recv(1024).decode())
+        toadd = {'s': c, 'name': name, 'id': round(time.time() * random.randint(1000, 9999))}
+        users.append(toadd)
+        print('Got connection from', addr)
+        t = threading.Thread(target=unos, args=(c, toadd['id'],name))
+        t.start()
+
+
+def unos(c, index, name):
+    while True:
+        novo = str(c.recv(1024).decode())
+        if novo=='':
+            novo="User disconnected: "
+        print(novo)
+        broad(novo, index)
+        if novo[:19] == "User disconnected: ":
+            for i in range(len(users)):
+                if index == users[i]['id']:
+                    users[i]['s'].close()
+                    del users[i]
+                    break
+            return
+
+def broad(msg, fake):
+    for i in range(len(users)):
+        if users[i]['id'] != fake:
+            users[i]['s'].send(msg.encode())
+
+
+try:
+    accConns(s)
+except KeyboardInterrupt:
+    s.shutdown(1)
+    s.detach()
+    s.close()
+    print("Bye bye")
+    exit(0)
+finally:
+    exit(0)
